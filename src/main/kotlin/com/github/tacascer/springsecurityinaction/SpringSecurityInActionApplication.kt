2d8ea97d99a6ea47f31419411827f19b5445624a
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-01-31 22:58:24 -0500

    chore: initial commit

---
+++ src/main/kotlin/com/github/tacascer/springsecurityinaction/SpringSecurityInActionApplication.kt
@@ -0,0 +1,11 @@
+package com.github.tacascer.springsecurityinaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityInActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityInActionApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/tacascer/springsecurityinaction/SpringSecurityInActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.tacascer.springsecurityinaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityInActionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
